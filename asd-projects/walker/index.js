9bbb0ccfd7f57fb728d12bdc5473ff555b2d7473
khamal22 <yyeah3198@gmail.com>
2022-06-07 21:00:00 +0000

    on to do 2

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -16,7 +16,7 @@ function runProgram(){
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on("keyDown", handleKeyDown);                           // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
