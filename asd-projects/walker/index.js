8492d23617c66cdd8efb7d8d2f6e5d3d3f1a2d88
khamal22 <yyeah3198@gmail.com>
2022-06-08 20:57:45 +0000

    finished to 5c

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -1,8 +1,8 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
-function runProgram(){
+
+function runProgram() {
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -10,13 +10,24 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+
   // Game Item Objects
 
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on("keyDown", handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on("keydown", handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  var KEY = {
+    "left": 37,
+    "right": 39,
+    "up": 38,
+    "down": 40,
+  }
+
+  var locationX = 0;
+  var locationY = 0;
+  var speedX = 0;
+  var speedY = 0;
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,22 +38,32 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+
 
   }
-  
+
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+    if (event.which === KEY.left) {
+      console.log("left pressed");
+    }
+    else if (event.which === KEY.right) {
+      console.log("right pressed");
+    }
+    else if (event.which === KEY.up) {
+      console.log("up pressed");
+    }
+    else if (event.which === KEY.down) {
+      console.log("down pressed")
+    }
   }
-
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
@@ -50,5 +71,13 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
-  
+  function repositonGameItem() {
+    locationX += speedX;
+    locationY += speedY;
+  }
+  function redrawDrawItem() {
+ $("#box").css("left", locationX);
+  }
+
+
 }
