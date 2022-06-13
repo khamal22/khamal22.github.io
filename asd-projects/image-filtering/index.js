2261aa0e7aea23b10157e85a296d760a91840e57
khamal22 <yyeah3198@gmail.com>
2022-06-13 21:00:52 +0000

    on todo 5

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -1,10 +1,10 @@
 // This is a small program. There are only two sections. This first section is what runs
 // as soon as the page loads and is where you should call your functions.
-$(document).ready(function(){
+$(document).ready(function () {
     const $display = $('#display');
 
     // Multiple TODOs: Call your apply function(s) here
-
+    applyFilter(reddify);
 
 
 
@@ -17,6 +17,32 @@ $(document).ready(function(){
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
+function applyFilter(filterFunction) {
+
+    for (var i = 0; i < image.length; i++) {
+       
+        var row = image[i]
+
+        for (var j = 0; j < row.length; j++){
+            
+            // grabing values from array
+            // ex "rgb(150, 150, 150)"
+            var rgbstring = image[i][j]
+
+            //converts a string to an array
+            var rgbNumbers = rgbStringToArray(rgbstring);
+            
+            //changes the value of the colors
+            filterFunction(rgbNumbers);
+
+            // converts array to string
+           image[i][j] = rgbArrayToString(rgbNumbers); 
+        }
+
+    }
+    
+    
+}
 
 
 // TODO 7: Create the applyFilterNoBackground function
@@ -26,7 +52,9 @@ $(document).ready(function(){
 
 
 // TODO 3: Create reddify function
-
+    function reddify(arr){
+        arr[RED] = 200
+    }
 
 // TODO 6: Create more filter functions
 
