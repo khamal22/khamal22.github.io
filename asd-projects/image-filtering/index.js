d7835998fbac77ee4118f0e227f686ba39c3fb80
khamal22 <yyeah3198@gmail.com>
2022-06-15 19:02:46 +0000

    finished image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -5,8 +5,9 @@ $(document).ready(function () {
 
     // Multiple TODOs: Call your apply function(s) here
     applyFilter(reddify);
-    applyFilter(decreaseBlue);
-    applyFilter(increaseGreenByBlue);
+    applyFilterNoBackground(decreaseBlue);
+    applyFilterNoBackground(increaseGreenByBlue);
+    
 
 
 
@@ -48,17 +49,48 @@ function applyFilter(filterFunction) {
 
 
 // TODO 7: Create the applyFilterNoBackground function
-    function applyFilterNoBackground(){
-        
+function applyFilterNoBackground(filterFunction) {
+
+
+    var backgroundColor = image[0][0];
+
+    for (var i = 0; i < image.length; i++) {
+
+        var row = image[i]
+
+        for (var j = 0; j < row.length; j++) {
+
+            // if statement to make sure the code only changes the mario.
+
+            if (image[i][j] !== backgroundColor ) {
+                // grabing values from array
+                // ex "rgb(150, 150, 150)"
+                var rgbstring = image[i][j]
+
+                //converts a string to an array
+                var rgbNumbers = rgbStringToArray(rgbstring);
+
+                //changes the value of the colors
+                filterFunction(rgbNumbers);
+
+                // converts array to string
+                image[i][j] = rgbArrayToString(rgbNumbers);
+            }
+
+        }
+
     }
+
+
+}
 
 // TODO 5: Create the keepInBounds function
 function keepInBounds(num) {
-    if (num < 0){
+    if (num < 0) {
         return 0;
 
     }
-    else if ( num > 255){
+    else if (num > 255) {
         return 255;
     }
     else {
@@ -74,11 +106,11 @@ function reddify(arr) {
 }
 
 // TODO 6: Create more filter functions
-function decreaseBlue(arr){
+function decreaseBlue(arr) {
     arr[BLUE] = keepInBounds(arr[BLUE] - 50);
 }
 
-function increaseGreenByBlue(arr){
+function increaseGreenByBlue(arr) {
     arr[GREEN] = keepInBounds(arr[BLUE] + arr[GREEN]);
 }
 
