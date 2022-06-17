2f2b9c7801c01397eb4050dd99c3d6c85aa38301
khamal22 <yyeah3198@gmail.com>
2022-06-17 21:03:44 +0000

    finished sorting

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -17,8 +17,7 @@ The CSS ids you will work with are:
 
 async function bubbleSort(array) {
     for (var i = 0; i < array.length - 1; i++) {
-
-        for (var j = array.length - 1; j > i + 1; j--) {
+        for (var j = array.length - 1; j >= i + 1; j--) {
            
             if (array[j].value < array[j - 1].value) {
                 swap(array, j, j - 1);
