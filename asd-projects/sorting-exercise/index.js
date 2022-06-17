78183efe28cf19820879d93396a80df6407ba48f
khamal22 <yyeah3198@gmail.com>
2022-06-17 20:52:00 +0000

    fixing bubble sort

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -16,13 +16,13 @@ The CSS ids you will work with are:
 // TODO 2: Implement bubbleSort
 
 async function bubbleSort(array) {
-    for (var i = 0; i < array.length - 1; i++){
+    for (var i = 0; i < array.length - 1; i++) {
 
-        for (var j = array.length - 1; j > i + 1; j--){
-            console.log({i,j});
+        for (var j = array.length - 1; j > i + 1; j--) {
+           
             if (array[j].value < array[j - 1].value) {
                 swap(array, j, j - 1);
-                
+
                 updateCounter(bubbleCounter);
                 await sleep();
             }
@@ -30,13 +30,44 @@ async function bubbleSort(array) {
     }
 
 
-    
+
 }
 
 // TODO 3: Implement quickSort
+async function quickSort( array, left, right) {
+    var index = await partition(array, left, right);
+    if (left < index -1){
 
+        await quickSort(array, left, index - 1);
+    }
+
+    if (right > index){
+        await quickSort(array, index, right);
+    }
+    return;
+}
 
 // TODOs 4 & 5: Implement partition
+async function partition(array, left, right) {
+
+    var pivot = array[Math.floor((right + left) / 2)].value;
+    while (left < right) {
+        while (array[left].value < pivot) {
+            left++;
+        }
+        while (array[right].value > pivot) {
+            right--;
+        }
+        if (left < right){
+            swap(array, left, right);
+            updateCounter(quickCounter);
+            await sleep();
+        }
+
+    }
+    return left + 1;
+
+}
 
 
 // TODO 1: Implement swap
