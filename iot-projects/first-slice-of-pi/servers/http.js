ea1887cc759c3f84b083cdad12385e61d8d45506
khamal22 <yyeah3198@gmail.com>
2022-11-29 18:32:16 -0600

    testing todo 2

--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,6 +1,14 @@
 const express = require('express'),
 	cors = require('cors');
 	
-	var app = express();
+	var app = express(cors);
+
+	app.get('/', function(req, res){
+		res.send('root accessed');
+	 });
 
 module.exports = app;
+
+
+
+//i have looked through all files//
