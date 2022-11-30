33c00565899d740be31f330b1fa92d7edb258a41
khamal22 <yyeah3198@gmail.com>
2022-11-29 18:35:47 -0600

    todo 2

--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,10 +1,11 @@
 const express = require('express'),
 	cors = require('cors');
 	
-	var app = express(cors);
+	var app = express();
+	app.use(cors());
 
 	app.get('/', function(req, res){
-		res.send('root accessed');
+		res.send('Some response for accessing the root');
 	 });
 
 module.exports = app;
