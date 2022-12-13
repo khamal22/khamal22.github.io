69c38a1f8926610ccfc98acbac62e36e7b0ee3dd
khamal22 <yyeah3198@gmail.com>
2022-12-13 17:38:52 -0600

    todo 4

--- iot-projects/first-slice-of-pi/routes/actuators.js
+++ iot-projects/first-slice-of-pi/routes/actuators.js
@@ -2,6 +2,20 @@ const express = require('express'),
 	router = express.Router(),
 	resources = require('./../resources/model');
 
-	
+	router.route('/').get(function (req, res, next) {
+		res.send(resources.pi.sensors);
+	 });
+	 
+	 router.route('/leds').get(function (req, res, next) {
+		res.send(resources.pi.sensors.leds);
+	 });
+
+	 router.route('/leds1/:id').get(function (req, res, next) {
+		res.send(resources.pi.actuators.leds1[req.params.id]);
+	});
+
+	router.route('/leds2/:id').get(function (req, res, next) {
+		res.send(resources.pi.actuators.leds2[req.params.id]);
+	});
 
 module.exports = router;
--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -3,9 +3,11 @@ var sensorRoutes = require("./../routes/sensors");
 const express = require("express"),
   cors = require("cors");
 
+  var actuatorRoutes = require('./../routes/actuators');
 var app = express();
 app.use(cors());
 app.use("/pi/sensors", sensorRoutes);
+app.use('/leds/sensors', sensorRoutes);
 
 app.get("/", function (req, res) {
   res.send("Some response for accessing the root");
